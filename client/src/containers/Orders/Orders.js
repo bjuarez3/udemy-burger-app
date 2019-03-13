2096a11034dd3ee141aa1ccb1228caf371a76952
Benjamin Juarez <ben95juarez@gmail.com>
2019-03-13 11:08:46 -0500

    commit

---
+++ client/src/containers/Orders/Orders.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount() {
+        axios.get('/orders')
+            .then(res => {
+                console.log(res.data)
+                this.setState({loading: false})
+            })
+            .catch(err => {
+                this.setState({loading: false})
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                <Order /> 
+                <Order />
+            </div>
+        )
+    }
+}
+
+export default withErrorHandler(Orders, axios);
